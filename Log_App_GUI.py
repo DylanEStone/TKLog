f438d41b7b614cbc671975c9ae47c7900a75c0bb
Dylan Stone <dylanevanstone@gmail.com>
2020-07-04 12:50:26 -0700

    Modification Funcitonality Added

--- Log_App_GUI.py
+++ Log_App_GUI.py
@@ -36,13 +36,10 @@ CREATE TABLE IF NOT EXISTS logs (
 """)
 
 # TODO: On submit, copy submitted fields to last comment field (Like SFOCLOG DOES)
-# Create Submit function for database
-def submit(doesnothing):
-    # create a database or connect to one
+def submit(doesnothing):    #i had to put a dummy parameter because enter was bound to submit... I am not sure why but it is ok
+    
     conn = sqlite3.connect('logs.db')
-    # create cursor
     c = conn.cursor()
-
     
     # Insert into table
     c.execute("INSERT INTO logs VALUES (:doyentry, :utcentry, :dssentry, :scentry, :enttype, :note)",
@@ -87,9 +84,135 @@ def openRecords():
         conn.commit()
         conn.close()
 
-    def modify():
+    def update(doesnothing):
         conn = sqlite3.connect('logs.db')
         c = conn.cursor()
+
+        record_id = oid_box.get()
+        c.execute("""
+            UPDATE logs SET
+            doy = :DOYEntry,
+            utc = :UTCEntry,
+            dss = :DSSEntry,
+            sc = :SCEntry,
+            enttype = :EntType,
+            note = :NoteEntry
+
+            WHERE oid = :oid
+        """,
+        {
+            'DOYEntry': DOYEntry_editor.get(),
+            'UTCEntry': UTCEntry_editor.get(),
+            'DSSEntry': DSSEntry_editor.get(),
+            'SCEntry': SCEntry_editor.get(),
+            'EntType': EntType_editor.get(),
+            'NoteEntry': NoteEntry_editor.get("1.0", 'end'),
+
+            'oid': record_id
+        })
+
+        conn.commit()
+        query_label.delete(1.0, END)
+        printLogs(c)
+        conn.close()
+
+        editor.destroy()
+        
+
+
+    def modify():
+        global editor
+        editor = Tk()
+        editor.title('Edit an entry')
+
+        editor.bind('<Return>', update)
+
+        conn = sqlite3.connect('logs.db')
+        c = conn.cursor()
+
+        record_id = oid_box.get()
+
+        # Query the database
+        c.execute("SELECT * FROM logs WHERE oid = " + record_id)
+        records = c.fetchall()
+
+        # Select record to modify
+        
+        # Create Global Variables for text box names
+        global DOYEntry_editor
+        global UTCEntry_editor
+        global DSSEntry_editor
+        global SCEntry_editor
+        global EntType_editor
+        global NoteEntry_editor
+        
+        ##################  Edit GUI   #######################
+        # DOY
+        DOYLabel_editor = Label(editor, text="DOY")
+        DOYEntry_editor = Entry(editor)
+
+        # UTC
+        UTCValue_editor = StringVar(editor)
+        UTCValue_editor.trace('w', limitSizeUTC)
+
+        UTCLabel_editor = Label(editor, text="UTC")
+        UTCEntry_editor = Entry(editor, textvariable=UTCValue)
+
+        #DSS
+        DSSValue_editor = StringVar(editor)
+        DSSValue_editor.trace('w', limitSizeDSS)
+
+        DSSLabel_editor = Label(editor, text="DSS")
+        DSSEntry_editor = Entry(editor, textvariable=DSSValue_editor)
+
+        # SC
+        SCValue_editor = StringVar(editor)
+        SCValue_editor.trace('w', limitSizeSC)
+
+        SCLabel_editor = Label(editor, text="S/C")
+        SCEntry_editor = Entry(editor, textvariable=SCValue_editor)
+
+        # ENTType
+        EntType_editor = StringVar(editor)   #FIXME: If DR, FR or Proc is selected, should promt user to enter numeber
+        EntTypeLabel_editor = Label(editor, text="EntType")
+        EntTypeOptions = [ "Log", "DR", "FR", "Proc" ]
+        EntTypeDropdown_editor = OptionMenu(editor, EntType_editor, *EntTypeOptions)
+        EntType_editor.set(EntTypeOptions[0]) # default value = Log
+
+        # Note
+        NoteLabel_editor = Label(editor, text="Entry")
+        NoteEntry_editor = Text(editor, height=5 , width=90)
+
+        
+
+        #############    Placement   ##################
+        DOYLabel_editor.grid(row=0, column=0)
+        DOYEntry_editor.grid(row=1, column=0)
+
+        UTCLabel_editor.grid(row=0, column=1)
+        UTCEntry_editor.grid(row=1, column=1)
+        UTCEntry_editor.focus()
+
+        DSSLabel_editor.grid(row=0, column=2)
+        DSSEntry_editor.grid(row=1, column=2)
+
+        SCLabel_editor.grid(row=0, column=3)
+        SCEntry_editor.grid(row=1, column=3)
+
+        EntTypeLabel_editor.grid(row=0, column=4)
+        EntTypeDropdown_editor.grid(row=1, column=4)
+
+        NoteLabel_editor.grid(row=2, columnspan=5)
+        NoteEntry_editor.grid(row=3, columnspan=5)
+
+        for record in records:
+            DOYEntry_editor.insert(0, record[0])
+            UTCEntry_editor.insert(0, record[1])
+            DSSEntry_editor.insert(0, record[2])
+            SCEntry_editor.insert(0, record[3])
+            EntType_editor.set(record[4])
+            NoteEntry_editor.insert(1.0, record[5])
+
 
         
 
@@ -125,7 +248,7 @@ def openRecords():
     delete_btn = Button(recordWindow, text="Delete", command=delete)
 
     # Modify Button
-    modify_btn = Button(recordWindow, text="Modify", command=delete)
+    modify_btn = Button(recordWindow, text="Modify", command=modify)
     
     oid_label = Label(recordWindow, text="ID")
     oid_box = Entry(recordWindow, width=5)
